bf2588898b53cc81676d7bd69d17797c5a815c36
lei.yu <lei.yu@msxf.com>
2016-05-12 16:44:22 +0800

    头部的初步修改

--- src/main/java/com/yulei/demo/repository/UserRepository.java
+++ src/main/java/com/yulei/demo/repository/UserRepository.java
@@ -21,6 +21,4 @@ public interface UserRepository extends BaseRepository<User, Long> {
     @Query(value = "select * from user u where u.deleted=:deleted and u.real_name like :search limit :begin,:rowCount", nativeQuery = true)
     List<User> findAllByDeleted(@Param("deleted") String deleted, @Param("begin") int begin, @Param("rowCount") int rowCount, @Param("search") String searchPhase);
 
-    @Query(value = "update user u set u.updated_by=:id1,u.deleted=1 where u.id=:id", nativeQuery = true)
-    void lockUser(@Param("id") Long id, @Param("id1") Long id1);
 }
